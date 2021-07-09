8914a1c9b5c784f0b41d20190595df538a798dde
Eideren <contact@eideren.com>
2021-07-09 03:07:13 +0200

    name: IEquatable for fast dictionary operations

--- Assets/Source/Integration/name.cs
+++ Assets/Source/Integration/name.cs
@@ -4,7 +4,7 @@
 
 
 
-    public readonly struct name
+    public readonly struct name : IEquatable<name>
     {
         public readonly String Value;
         public name(String v) => Value = v;
@@ -13,8 +13,15 @@
         public static implicit operator name(String v) => new name(v);
         public static implicit operator name(string v) => new name(v);
 
+        public bool Equals( name other ) => other == this;
         public override bool Equals( object? obj ) => Value.Equals( obj );
-        public override int GetHashCode() => Value.GetHashCode();
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+
+
+
         public static bool operator ==(name a, name b) => (a.Value == "None" && b.Value == "") || (a.Value == "" && b.Value == "None") || string.Equals( a.Value, b.Value, StringComparison.InvariantCultureIgnoreCase );
         public static bool operator !=(name a, name b) => (a == b) == false;
         public override string ToString() => Value.ToString();
