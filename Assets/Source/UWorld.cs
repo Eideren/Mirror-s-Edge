d00c29a79186dd1152f90b497f2c74aa813ea29d
Eideren <contact@eideren.com>
2021-07-17 07:43:13 +0200

    Split pawn link from UWorld

--- Assets/Source/UWorld.cs
+++ Assets/Source/UWorld.cs
@@ -7,7 +7,6 @@
     using Source;
     using TdGame;
     using Tp;
-    using UnityEngine;
     using UnityEngine.Events;
     using UnityEngine.SceneManagement;
     using V3 = UnityEngine.Vector3;
@@ -38,141 +37,6 @@
         static UWorld()
         {
             UWorldBridge.GetUWorld = () => Instance;
-        }
-
-
-
-        class PawnLink : IProcessor
-        {
-            public TdPawn Pawn;
-            AnimationPlayer _1pPlayer;
-            AnimNodeEditor.AnimNodeEditorWindow _window;
-            UnityEngine.Camera _unityCam;
-
-
-
-            public void Update(float deltaTime)
-            {
-                ComputeVelocity_Stub(Pawn, deltaTime, Pawn.GroundSpeed, Pawn.PhysicsVolume.GroundFriction, 0, true );
-                // Not exactly like this, depends on more stuff, but good approximation
-                Pawn.Location += (Pawn.Velocity + Pawn.PhysicsVolume.ZoneVelocity * 25f) * deltaTime;
-
-                if( _1pPlayer == null )
-                {
-                    var clips = Resources.LoadAll<AnimationClip>( "Animations/AS_C1P_Unarmed/" );
-                    Asset.UScriptToUnity.TryGetValue( Pawn.Mesh1p.SkeletalMesh, out var unityObject );
-                    _1pPlayer = new AnimationPlayer( clips, Asset.Get_AS_C1P_Unarmed, Pawn.Mesh1p.Animations, ((SkinnedMeshRenderer)unityObject).transform.parent.gameObject, Pawn );
-                    _window = AnimNodeEditor.AnimNodeEditorWindow.CreateInstance<AnimNodeEditor.AnimNodeEditorWindow>();
-                    _window.LoadFromNode( Pawn.Mesh1p.Animations );
-                    _window.Show();
-                }
-                _1pPlayer.Sample( deltaTime );
-                if( _unityCam == null )
-                {
-                    _unityCam = UnityEngine.Camera.main;
-                    if( _unityCam == null )
-                    {
-                        GameObject camGo = new GameObject();
-                        _unityCam = camGo.AddComponent<UnityEngine.Camera>();
-                    }
-
-                    ( Pawn.Controller as PlayerController ).SpawnPlayerCamera();
-                }
-
-                if( ( Pawn.Controller as PlayerController ).PlayerCamera is Camera cam )
-                {
-                    var camPov = cam.CameraCache.POV;
-                    _unityCam.transform.SetPositionAndRotation( camPov.Location.ToUnityPos(), (Quaternion)camPov.Rotation );
-                    #warning probably switch things around to ensure camera doesn't lag one frame behind animation
-                }
-            }
-
-
-
-            public void OnDestroy()
-            {
-                _window?.Close();
-                _window = null;
-
-                string appendedMessage = "Native unimplemented calls:\n" + string.Join( "\n", NativeMarkers.Marked.Select( x => $"{x.FilePath} - {x.Member}:{x.Line}" ).OrderBy( x => x ) );
-                LogError(appendedMessage);
-            }
-
-
-
-
-            void ComputeVelocity_Stub( TdPawn p, float likelyTimeDelta, float mod_param_speed, float param_friction, int param_bStdDeceleration, bool bFixedTimeDeceleration )
-            {
-                var mod_currentMove__PawnMobility = p.GetMobilityMultiplier() * ( p.Moves[ (int) p.MovementState ]?.SpeedModifier ?? 1f );
-                var mod_currentMove__PawnMobility__paramSpeed = mod_currentMove__PawnMobility * mod_param_speed;
-                if ( bFixedTimeDeceleration && p.Acceleration.X == 0.0 && p.Acceleration.Y == 0.0 && p.Acceleration.Z == 0.0 )
-                {                                       // 
-                                                        // deceleration == true && acceleration == 0
-                                                        // 
-                    var remainingTime = likelyTimeDelta;    // Apply deceleration logic, also runs when idle
-                    var velocityBeforeDeceleration = p.Velocity;
-                    var velocityAfterDeceleration = new Object.Vector();
-                    if ( likelyTimeDelta > 0.0 )
-                    {
-                        var brakingFrictionStrength = p.BrakingFrictionStrength;
-                        while ( true )
-                        {                               // 
-                                                        // Every loop reduce by 0.0299999 until 'loop*0.0299999' >= 'someVectorMult' at which point reduce by the rest and stop loop
-                                                        // This might be because decelerating using just the deltatime might diverge widely based on the deltatime value
-                                                        // 
-                            var timeSlice = 0.029999999f;
-                            if ( remainingTime <= 0.029999999f )
-                                timeSlice = remainingTime;// 
-                                                        // Take the minimum between 0.0299... and remainingTime
-                                                        // 
-                            var tempVelY2 = p.Velocity.Y;
-                            var reductionLeft = remainingTime - timeSlice;// 
-                                                        // 
-                                                        // 
-                            var tempVelX = (float)((float)((float)(p.Velocity.X * 2.0) * timeSlice) * param_friction) * brakingFrictionStrength;
-                            var miscUtility3Float = (float)((float)((float)(p.Velocity.Z * 2.0) * timeSlice) * param_friction) * brakingFrictionStrength;
-                            var tempVelY = p.Velocity.Y - (float)((float)((float)((float)(tempVelY2 * 2.0) * timeSlice) * param_friction) * brakingFrictionStrength);
-                            var tempVelZ2 = p.Velocity.Z - miscUtility3Float;
-                            p.Velocity.X = p.Velocity.X - tempVelX;
-                            p.Velocity.Y = tempVelY;
-                            p.Velocity.Z = tempVelZ2;// 
-                                                        // 
-                                                        // p.Velocity -= p.Velocity * 2.0 * timeSlice * anotherVectorMult * brakingFrictionStrength
-                                                        // 
-                                                        // 
-                            if ( (float)((float)((float)(p.Velocity.Y * velocityBeforeDeceleration.Y) + (float)(p.Velocity.Z * velocityBeforeDeceleration.Z)) + (float)(p.Velocity.X * velocityBeforeDeceleration.X)) > 0.0 )
-                            {                           // 
-                                                        // That's a dot comp, if previous velocity and current are in the same direction:
-                                var scaledVelZ = 0.0;
-                                velocityAfterDeceleration.X = (float)((float)(p.Velocity.X * timeSlice) * (float)(1.0 / likelyTimeDelta)) + velocityAfterDeceleration.X;
-                                velocityAfterDeceleration.Y = (float)((float)(p.Velocity.Y * timeSlice) * (float)(1.0 / likelyTimeDelta)) + velocityAfterDeceleration.Y;
-                                velocityAfterDeceleration.Z = (float)((float)(p.Velocity.Z * timeSlice) * (float)(1.0 / likelyTimeDelta)) + velocityAfterDeceleration.Z;// 
-                                                        // velocityAfterDeceleration += (p.Velocity * timeSlice) * (1.0 / likelyTimeDelta);
-                                                        // 
-                            }
-                            if ( reductionLeft <= 0.0 )
-                                break;
-                            remainingTime = reductionLeft;
-                        }
-                    }
-                    p.Velocity = velocityAfterDeceleration;
-                    if ( (float)((float)((float)(p.Velocity.Y * velocityBeforeDeceleration.Y) + (float)(p.Velocity.Z * velocityBeforeDeceleration.Z)) + (float)(p.Velocity.X * velocityBeforeDeceleration.X)) < 0.0
-                      || (float)((float)((float)(p.Velocity.X * p.Velocity.X) + (float)(p.Velocity.Y * p.Velocity.Y)) + (float)(p.Velocity.Z * p.Velocity.Z)) < 100.0 )
-                    {                                   // 
-                                                        // velocity flipped direction or is lower than 100
-                                                        // => set velocity to zero
-                        p.Velocity.X = 0.0f;
-                        p.Velocity.Y = 0.0f;
-                        p.Velocity.Z = 0.0f;
-                    }
-                }
-                p.Velocity *= 1.0f - (param_bStdDeceleration * likelyTimeDelta * param_friction);
-                p.Velocity += p.Acceleration * likelyTimeDelta;
-                if( (float) ( (float) ( (float) ( p.Velocity.X * p.Velocity.X ) + (float) ( p.Velocity.Y * p.Velocity.Y ) ) + (float) ( p.Velocity.Z * p.Velocity.Z ) ) > (float) ( mod_currentMove__PawnMobility__paramSpeed * mod_currentMove__PawnMobility__paramSpeed ) )
-                {
-                    p.Velocity = p.Velocity.SafeNormal() * mod_currentMove__PawnMobility__paramSpeed;
-                }
-            }
         }
 
 
@@ -521,6 +385,13 @@
                 newOuter = _defaultOuter;// *(_E_struct_AActor **)&thisUWorldCopy->UnknownData_[24];
             // Need to verify this as well
             var constructedActor = SpawnClassCopy.New( newOuter );////E_UObject_StaticConstructObject(SpawnClassCopy, newOuter, a3, SLODWORD(a4), 0, 1, actorTemplateCopy, 0, 0);
+
+            // Not sure where this is supposed to trigger, not sure that it matters anyway
+            foreach( var component in constructedActor.Components )
+            {
+                component.Owner = constructedActor;
+            }
+            
             /*constructedActorCopy = constructedActor;
             if ( dword_2018700 )
                 sub_B81580(newOuter);
