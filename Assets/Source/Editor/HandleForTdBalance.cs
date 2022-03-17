6942dff228f29ae782314ac7775557b9059b9808
Eideren <contact@eideren.com>
2022-03-17 08:00:26 +0100

    TdBalanceWalkVolume

---
+++ Assets/Source/Editor/HandleForTdBalance.cs
@@ -0,0 +1,27 @@
+﻿namespace MEdge.Source.Editor
+{
+	using Editor = UnityEditor.Editor;
+	using UnityEditor;
+	
+	
+	[CustomEditor(typeof(UnityTdBalanceWalkVolume))]
+	public class HandlesForTdBalance : Editor
+	{
+		public void OnSceneGUI()
+		{
+			var tdLadder = (UnityTdBalanceWalkVolume)target;
+			for( int i = 0; i < tdLadder.SplineControls.Count; i++ )
+			{
+				var sPoint = tdLadder.SplineControls[ i ];
+				sPoint = tdLadder.Transform( sPoint );
+				EditorGUI.BeginChangeCheck();
+				var nPoint = Handles.PositionHandle( sPoint, tdLadder.transform.rotation );
+				if (EditorGUI.EndChangeCheck() && sPoint != nPoint )
+				{
+					Undo.RecordObject (tdLadder, $"Moved spline point on {tdLadder.gameObject.name}");
+					tdLadder.SplineControls[ i ] = tdLadder.InverseTransform( nPoint );
+				}
+			}
+		}
+	}
+}
