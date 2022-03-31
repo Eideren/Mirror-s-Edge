c9e59cb56aeab1c93ff45eb97dbe7ca3f64c83d5
Eideren <contact@eideren.com>
2022-03-31 17:24:26 +0200

    Clear some warnings

--- Assets/Source/Notes.cs
+++ Assets/Source/Notes.cs
@@ -1,5 +1,3 @@
 ﻿#warning Equality comp with class def
 #warning TdSwing: Look at StartMove, weird conversion, 'SwingAngle = ((float)(Clamp(((int)(SwingAngle)), ((int)(-1.20f)), ((int)(-0.70f)))));' maybe just dev' error since FClamp exist
 #warning fix struct construction
-#warning cpptext, nowhere to be found for some reason
-#warning Write up on reversing
