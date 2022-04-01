c747463fbaa837437f090fe6ce1d79ef6629bd6a
Eideren <contact@eideren.com>
2022-04-01 15:23:47 +0200

    Gizmo for spawn point

--- Assets/SpawnPoint.cs
+++ Assets/SpawnPoint.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 namespace MEdge
 {
     using Core;
@@ -20,5 +22,16 @@ namespace MEdge
 
 
         public Object GetUObject() => _playerStart ??= new PlayerStart();
+
+
+        public void OnDrawGizmos()
+        {
+            // This is not exact, just an idea of how large of an area the player character likely needs to spawn properly
+            var size = new Vector3(1, 2, 1);
+            var pos = this.transform.position + Vector3.up * size.y * 0.5f;
+            Gizmos.color = (Physics.CheckBox(pos, size * 0.5f) ? Color.red : Color.yellow) * new Color(1f, 1f, 1f, 0.25f);
+            Gizmos.DrawCube( pos, size );
+            Gizmos.DrawWireCube( pos, size );
+        }
     }
 }
